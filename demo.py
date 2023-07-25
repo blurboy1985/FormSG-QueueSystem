a37b1783108a067ef5d40bcf2805f7cd791080da
Daniel Quek <daniel.tianwen@gmail.com>
2023-07-25 22:47:57 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-from streamlit_autorefresh import st_autorefresh
 import streamlit as st
 
 # Check if 'queue_counter' is already in the session state
@@ -20,15 +19,13 @@ if st.session_state.queue_counter <= 10:
     registration_link = f"https://form.gov.sg/64bfbe8bf8b1ef0011d9a0df?64bfbebb7b53ad0011459dde={st.session_state.queue_counter}"
     
     # Using markdown to style the link as a button and display the queue counter without target="_blank"
-    button_code = f'<a href="{registration_link}" target="_self" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
+    button_code = f'<a href="{registration_link}" target="_blank" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
     st.markdown(button_code, unsafe_allow_html=True)
     
     st.write(f"Queue Number: {st.session_state.queue_counter}")
 
     # Increment the queue counter for the next person
     st.session_state.queue_counter += 1
-    st_autorefresh(interval=1, limit=1)  # Refresh the page every 1 second
-
 
 else:
     st.write("""
