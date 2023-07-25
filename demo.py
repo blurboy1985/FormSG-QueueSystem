2cc4a331a4f301eec8b75e6bc4e5b021f73534ee
Daniel Quek <daniel.tianwen@gmail.com>
2023-07-25 22:53:25 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,7 +19,7 @@ if st.session_state.queue_counter <= 10:
     registration_link = f"https://form.gov.sg/64bfbe8bf8b1ef0011d9a0df?64bfbebb7b53ad0011459dde={st.session_state.queue_counter}"
     
     # Using markdown to style the link as a button and display the queue counter without target="_blank"
-    button_code = f'<a href="{registration_link}" target="_blank" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
+    button_code = f'<a href="#" onclick="event.preventDefault(); {st._get_report_ctx().enqueue(st.experimental_rerun)}" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
     st.markdown(button_code, unsafe_allow_html=True)
     
     st.write(f"Queue Number: {st.session_state.queue_counter}")
@@ -32,3 +32,4 @@ else:
     Due to overwhelming response, the event has been fully registered.
     You may email to [abc@gov.sg](mailto:abc@gov.sg) to check if there are any more vacancies for the event.
     """)
+
