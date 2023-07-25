b84a9cb7366bae9136bca11fb9a92ce5372aedc1
Daniel Quek <daniel.tianwen@gmail.com>
2023-07-25 23:00:05 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,9 +19,7 @@ if st.session_state.queue_counter <= 10:
     registration_link = f"https://form.gov.sg/64bfbe8bf8b1ef0011d9a0df?64bfbebb7b53ad0011459dde={st.session_state.queue_counter}"
     
     # Using markdown to style the link as a button and display the queue counter without target="_blank"
-    import json
-
-    button_code = f'<a href="#" onclick="event.preventDefault(); {json.dumps(st._get_report_ctx().enqueue(st.experimental_rerun))}()" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
+    button_code = f'<a href="{registration_link}" target="_blank" style="padding: 10px 20px; background-color: #FF2E63; color: white; border-radius: 4px; text-decoration: none;">Register</a>'
     st.markdown(button_code, unsafe_allow_html=True)
     
     st.write(f"Queue Number: {st.session_state.queue_counter}")
